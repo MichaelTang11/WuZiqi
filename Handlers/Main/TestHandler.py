cc41ab9dd148ad76282cb81c90912de9729b882f
Michael <1091149562@qq.com>
2019-03-05 13:59:43 +0800

    测试debug

--- Handlers/Main/TestHandler.py
+++ Handlers/Main/TestHandler.py
@@ -1,10 +1,11 @@
-#测试用handler
+# 测试用handler
 import tornado.web
-from Methods.ConnectDB import cursor
 
 
 class TestHandler(tornado.web.RequestHandler):
-    def get(self, *args, **kwargs):
-        userId="3"
-        friendId="1"
-        cursor.execute("INSERT INTO message_friend_list (user_id, friend_id) values(%s,%s)", (userId, friendId))
+    def post(self, *args, **kwargs):
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Headers', 'x-requested-with')
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
+        message = self.get_argument("message")
+        print(message)
