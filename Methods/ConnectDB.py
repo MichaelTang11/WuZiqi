92f758d8f24cc93632babc98f44bb18089c237a3
Michael <1091149562@qq.com>
2019-03-05 14:06:23 +0800

    测试debug

--- Methods/ConnectDB.py
+++ Methods/ConnectDB.py
@@ -1,5 +1,6 @@
 import pymysql
 
-con = pymysql.connect(host='127.0.0.1', user='root', port=3306, database='wuziqi', charset='utf8', password='TANGYuxing1111')
+con = pymysql.connect(host='127.0.0.1', user='root', port=3306, database='wuziqi', charset='utf8',
+                      password='TANGYuxing1111')
 con.autocommit(1)
 cursor = con.cursor(cursor=pymysql.cursors.DictCursor)
