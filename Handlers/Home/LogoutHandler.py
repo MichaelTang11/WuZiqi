19c488c27de294d0799f5cdb5aec479a45cf91c0
Michael <1091149562@qq.com>
2019-03-15 18:41:29 +0800

    测试debug

--- Handlers/Home/LogoutHandler.py
+++ Handlers/Home/LogoutHandler.py
@@ -8,10 +8,16 @@ from Methods.ConnectDB import cursor
 class LogoutHandler(tornado.web.RequestHandler):
     def post(self, *args, **kwargs):
         userId = self.get_secure_cookie("userId").decode("utf-8")
-        self.clear_cookie('userId')
         returnJson = {"status": "00"}
         self.write(json.dumps(returnJson, ensure_ascii=False))
         cursor.execute("UPDATE user SET login_state=0,state=0 WHERE user_id=%s", userId)
+        tableId = self.get_secure_cookie("tableId")
+        if tableId is not None:
+            cursor.execute("UPDATE game_table_info SET left_player_id=NULL WHERE table_id=%s AND left_player_id=%s",
+                           (tableId, userId))
+            cursor.execute("UPDATE game_table_info SET right_player_id=NULL WHERE table_id=%s AND right_player_id=%s",
+                           (tableId, userId))
         for key in HomeSocketCash:
             HomeSocketCash[key].refreshFriendList()
+        self.clear_all_cookies()
         logging.info("cookie清除成功！")
--- Handlers/Main/TestHandler.py
+++ Handlers/Main/TestHandler.py
@@ -4,8 +4,4 @@ import tornado.web
 
 class TestHandler(tornado.web.RequestHandler):
     def post(self, *args, **kwargs):
-        self.set_header('Access-Control-Allow-Origin', '*')
-        self.set_header('Access-Control-Allow-Headers', 'x-requested-with')
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
-        message = self.get_argument("message")
-        print(message)
+        self.clear_all_cookies()
