d341c321d2440f1d862b2b17cea856bb9000400a
Michael <1091149562@qq.com>
2019-04-08 10:00:16 +0800

    更改数据库密码

--- Methods/ConnectDB.py
+++ Methods/ConnectDB.py
@@ -1,6 +1,6 @@
 import pymysql
 
 con = pymysql.connect(host='127.0.0.1', user='root', port=3306, database='wuziqi', charset='utf8',
-                      password='usbw')
+                      password='TANGYuxing1111')
 con.autocommit(1)
 cursor = con.cursor(cursor=pymysql.cursors.DictCursor)
